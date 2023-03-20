4bfef80edb9df7e48e380221a1b97845cf85a8e4
ArthurKhalifa <arthursantosae@hotmail.com>
2023-03-20 10:17:40 -0300

    Courses-Web

---
+++ script.js
@@ -0,0 +1,14 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const rightH = document.getElementsByClassName('righth')[0]
+
+toggleButton.addEventListener('click', () => {
+    rightH.classList.toggle('active')
+})
+
+// *********************************
+
+const menu = document.getElementById("botao");
+
+menu.onclick = function(){
+    menu.classList.toggle("openmenu");
+}
